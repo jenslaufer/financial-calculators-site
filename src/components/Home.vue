f7dbd08a4e13071c858ace88cb59ba58115e1925
Jens Laufer <jens.laufer@solytics.de>
2026-02-04 09:46:15 +0100

    initial site

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex flex-col w-sreen h-screen justify-center items-center">
+    <h1 class="text-4xl font-bold mb-8">{{ headline }}
+    </h1>
+    <p class="text-lg mb-4">{{ uvp }}</p>
+  </div>
+</template>
+<script setup>
+const props = defineProps({
+  headline: {
+    type: String,
+    required: true
+  },
+  uvp: {
+    type: String,
+    required: true
+  }
+});
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createWebHashHistory, createRouter } from 'vue-router'
 
-createApp(App).mount('#app')
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes: [
+        {
+            path: '',
+            component: () => import('./components/Home.vue'),
+            props: {
+                headline: 'Welcome to Your Financial Calculators Site',
+                uvp: 'Use our tools to get your finances right'
+            }
+        }
+    ]
+})
+
+
+createApp(App)
+    .use(router)
+    .mount('#app')
+
