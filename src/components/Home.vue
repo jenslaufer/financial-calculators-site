94fd7c1e1aab2f9aa8c0a6c18b79fd4edcfd58f5
Jens Laufer <jens.laufer@solytics.de>
2026-02-04 09:58:58 +0100

    moved forward

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex flex-col w-sreen h-screen justify-center items-center">
-    <h1 class="text-4xl font-bold mb-8">{{ headline }}
+    <h1 class="text-3xl font-bold mb-8">{{ headline }}
     </h1>
-    <p class="text-xl mb-3">{{ uvp }}</p>
+    <p class="text-lg mb-3">{{ uvp }}</p>
   </div>
 </template>
 <script setup>
--- src/main.js
+++ src/main.js
@@ -10,8 +10,8 @@ const router = createRouter({
             path: '',
             component: () => import('./components/Home.vue'),
             props: {
-                headline: 'Bring Precision to Your Loans, Investments, and Financial Affairs',
-                uvp: 'Use our suite of financial calculators to make informed decisions with ease and accuracy.',
+                headline: 'Get your financials in order...',
+                uvp: 'Gain control of your loans, mortgages, stocks, and other financials with our calculators',
             }
         }
     ]
